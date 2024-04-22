953245fc05d94d5fb8e1c289621103a6b2662d8f
MtLuth <111564276+MtLuth@users.noreply.github.com>
2024-04-23 01:25:14 +0700

    Add activity_main_screen_app.xml and header_frame_layout.xml

---
+++ app/src/main/java/com/example/torchvisionapp/fragment/HeaderFragment.java
@@ -0,0 +1,59 @@
+package com.example.torchvisionapp.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import com.example.torchvisionapp.R;
+import com.example.torchvisionapp.databinding.FragmentHeaderBinding;
+
+public class HeaderFragment extends Fragment {
+
+    String title;
+
+    public HeaderFragment (String title) {
+        this.title = title;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        FragmentHeaderBinding binding;
+        binding = DataBindingUtil
+                .inflate(LayoutInflater.from(container.getContext()),
+                                R.layout.fragment_header,
+                                container,
+                                false
+                        );
+        ViewHolder holder = new ViewHolder();
+        holder.headerTitle = (TextView) binding.textHeader;
+        holder.headerTitle.setText(title);
+
+        return binding.getRoot();
+    }
+
+    private static class ViewHolder {
+        TextView headerTitle;
+        ImageView btnSort;
+        ImageView btnSearch;
+    }
+}
