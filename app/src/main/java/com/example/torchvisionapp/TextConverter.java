6e6c72c91b9e83a231c3c2441aa5fb9c8540ca70
hoangkhai <hoangkhai178@gmail.com>
2024-04-24 20:06:12 +0700

    capture

---
+++ app/src/main/java/com/example/torchvisionapp/TextConverter.java
@@ -0,0 +1,14 @@
+package com.example.torchvisionapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TextConverter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_converter);
+    }
+}
