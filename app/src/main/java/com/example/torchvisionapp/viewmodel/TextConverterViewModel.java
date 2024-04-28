1980093b2acd9ed6022f87dd00822663adec4d66
MtLuth <111564276+MtLuth@users.noreply.github.com>
2024-04-28 21:47:10 +0700

    commit

---
+++ app/src/main/java/com/example/torchvisionapp/viewmodel/TextConverterViewModel.java
@@ -0,0 +1,41 @@
+package com.example.torchvisionapp.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+import com.example.torchvisionapp.R;
+import com.example.torchvisionapp.model.FileItem;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TextConverterViewModel extends AndroidViewModel {
+    private ArrayList<File> listFolders;
+    private ArrayList<FileItem> listFileItem;
+    public TextConverterViewModel(@NonNull Application application) {
+        super(application);
+    }
+    public ArrayList<FileItem> loadFolderList(File path) {
+        listFolders = new ArrayList<>();
+        listFileItem = new ArrayList<>();
+        FileExplorer fileExplorer = new FileExplorer();
+        listFolders = fileExplorer.loadExistingFolderFromPath(path.getPath());
+        if (listFolders != null) {
+            for (File f: listFolders) {
+                if (f.isDirectory()) {
+                    FileItem item = new FileItem();
+                    item.setName(f.getName());
+                    item.setIcon(R.drawable.iconfolder_actived);
+                    item.setPath(f.getPath());
+                    item.setType("directory");
+                    item.setStatus(fileExplorer.countNumberOfFileInDirectory(f.getPath())+" files");
+                    listFileItem.add(item);
+                }
+            }
+        }
+        return listFileItem;
+    }
+}
