f01f1c4bc4badda25c145e3eb22cf5d45300fb9a
minhtrieucoder <tr.minhtrieu.hcmute@gmail.com>
2024-04-28 16:15:12 +0700

    modify

--- app/src/main/java/com/example/torchvisionapp/ocr/ModelExecutionResult.kt
+++
@@ -1,9 +0,0 @@
-package com.example.torchvisionapp.ocr
-
-import android.graphics.Bitmap
-
-data class ModelExecutionResult(
-    val bitmapResult: Bitmap,   // a image for result
-    val executionLog: String,
-    val itemsFound: Map<String, Int>,   // a map between words and colors of the items found.
-)
